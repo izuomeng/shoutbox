fdcaf394c24475fa7a63550eae450ff802efc221
izuomeng <dmate@foxmail.com>
2017-10-17 12:59:28 +0800

    validate post title

--- routes/entries.js
+++ routes/entries.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Entry = require('../lib/entry');
+var validate = require('../lib/middleware/validate');
 
 router.get('/', function(req, res, next) {
     Entry.getRange(0, -1, function(err, entries) {
@@ -18,19 +19,23 @@ router.get('/post', function(req, res) {
         title: 'Post'
     });
 });
-router.post('/post', function(req, res, next) {
-    var data = req.body,
-        entry = new Entry({
-            username: res.locals.user.name,
-            title: data.entry_title,
-            body: data.entry_body
-        })
-    entry.save(function(err) {
-        if (err) {
-            return next(err);
-        }
-        res.redirect('/')
-    });
-});
+router.post('/post', 
+    validate.required('entry_title'), 
+    validate.lengthAbove('entry_title', 4), 
+    function(req, res, next) {
+        var data = req.body,
+            entry = new Entry({
+                username: res.locals.user.name,
+                title: data.entry_title,
+                body: data.entry_body
+            })
+        entry.save(function(err) {
+            if (err) {
+                return next(err);
+            }
+            res.redirect('/')
+        });
+    }
+);
 
 module.exports = router;
